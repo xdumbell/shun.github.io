dd7ddfbaaac4ff8dc5a0b87fa5c011391c963775
Shun <124486051+xdumbell@users.noreply.github.com>
2024-07-28 23:53:47 +0800

    Add files via upload

---
+++ assets/projects.js
@@ -0,0 +1,34 @@
+export default [
+  {
+    title: "Fruit Inventrory System",
+    description:
+      "This system is fruit inventory, it is usable in fruit stores where the user can mamage the fruits either add or remove fruits",
+    image:
+       "assets/fruitssystem.jpeg",
+
+  },
+  {
+    title: "Login Form Attendance System",
+    description:
+      "This is the log in page which I created for our employee attendance system.",
+    image:
+      "assets/loginform.jpeg",
+    
+  },
+  {
+    title: "Time Checker",
+    description:
+      "This is the example of an attendance system where the user can time in and when the user can time out, it will be calculated and will display.",
+    image:
+      "assets/time.jpeg",
+    
+  },
+  {
+    title: "Lone Debt Bank",
+    description:
+      "It is used to determine the time and date of when the user starts the loan and it's due. And the birthdate of the user if the user is under age or over age.",
+    image:
+      "assets/loan.jpeg",
+    
+  },
+];
